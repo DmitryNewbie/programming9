d970b5568dc13b65c995b6ac89b88224c8e4d3ea
Dmitry <zimzulka1@gmail.com>
2023-10-30 00:21:36 +0400

    first commit

---
+++ з1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число");
+int number = int.Parse(Console.ReadLine());
+
+void count (int number)
+{
+    if(number == 0)
+    return;
+    Console.Write(number);
+    count(number -1);
+}
+count(number);
---
+++ з2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число M");
+int numberM = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numberN = int.Parse(Console.ReadLine());
+
+void SumNumber (int numberM, int numberN, int sum)
+{
+    if(numberM > numberN)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+    sum += numberM++;
+    
+    SumNumber( numberM, numberN, sum);
+}
+SumNumber(numberM, numberN, 0 );
